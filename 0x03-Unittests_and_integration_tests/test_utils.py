ecaca86542adec6fa8273e444a2d97fa8ef29b1d
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-14 16:49:51 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,7 +43,8 @@ class TestGetJson(unittest.TestCase):
         
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
-          class TestClass:
+        
+        class TestClass:
             def a_method(self):
                 return 42
 
@@ -51,7 +52,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-          with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
             test_class = TestClass()
             result1 = test_class.a_property
             result2 = test_class.a_property
