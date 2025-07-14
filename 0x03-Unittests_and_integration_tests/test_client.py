2402b571f377269923dae830a4abc769e87fec8a
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-14 17:21:20 +0100

    hot fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import unittest
-from parameterized import parameterized, parameterized_class
+from parameterized import parameterized
+from parameterized import parameterized_class
 from unittest.mock import patch, PropertyMock,Mock
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
 
