f062ac54257873cbb6a488f206329caae688e1be
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-15 10:37:24 +0100

    Defined data Models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    profile_image = models.ImageField(upload_to="profile_images/", null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
+
+
+class Conversation(models.Model):
+  participate = models.ManyToManyField(User, related_name="conversations")
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+        return f"Conversation {self.id}"
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    body = models.TextField()
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.id} from {self.sender} in {self.conversation}"
+    
