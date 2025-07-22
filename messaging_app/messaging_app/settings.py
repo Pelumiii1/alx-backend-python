c180388cfa7220b79549c18bf74101ce903c545f
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-22 15:58:44 +0100

    hot fix

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -56,6 +56,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
