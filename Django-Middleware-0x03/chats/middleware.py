7ca04d0fd950d4ee7dab56d36ca3326d65437114
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-23 16:38:32 +0100

    Logging User Requests(Basic Middleware)

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,23 +1,12 @@
 from datetime import datetime
-import logging
 
-logger = logging.getLogger('request_logger')
-if not logger.hasHandlers():
-    handler = logging.FileHandler('requests.log')
-    formatter = logging.Formatter('%(message)s')
-    
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
-    
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
-        self.get_reponse = get_response
-        
-    def __call__(self,request):
-        user = request.user if request.user.is_authenticated else "Anonymous"
-        log_message = f"{datetime.now()}- User:{user} - Path:{request.path}"
-        logging.info(log_message)
-        
-        response = self.get_reponse(request)
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        with open('requests.log', 'a') as f:
+            f.write(f"{datetime.now()} - User: {user} - Path: {request.path}\n")
+        response = self.get_response(request)
         return response
