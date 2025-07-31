5564c80f6f8430b7fe2b43a863808bdc87b94ef2
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-07-31 23:01:33 +0100

    Custom ORM Manager for Unread Messages

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.db.models import Q
 
-class MessageManager(models.Manager):
+class UnreadMessagesManager(models.Manager):
     def unread_for_user(self, user):
         return self.get_queryset().filter(receiver=user).exclude(read_receipts__user=user)
